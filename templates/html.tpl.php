e3fad96db9a3eb45bc1c25cd5f1507528d5331c6
Mark Baggett <mbagget1@utk.edu>
2016-05-13 09:58:24 -0400

    Add Google Tag Manager tracking code.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -14,5 +14,15 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MB99NS"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MB99NS');</script>
+<!-- End Google Tag Manager -->
+
 </body>
 </html>
